c96791c528e457d780077a401d148f9f902ea71d
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-07-21 14:42:29 +0530

    Added all the files

---
+++ script.js
@@ -0,0 +1,36 @@
+function blue() {
+  //accessing the h1 element
+  let color = "blue";
+  let h1 = document.querySelector("h1");
+  h1.style.color = color;
+  //accessing the h3 element
+  let h3 = document.querySelector("h3");
+  h3.style.color = color;
+  //accessing the h4 element
+  let h4 = document.querySelector("h4");
+  h4.style.color = color;
+}
+function red() {
+  //accessing the h1 element
+  let color = "red";
+  let h1 = document.querySelector("h1");
+  h1.style.color = color;
+  //accessing the h3 element
+  let h3 = document.querySelector("h3");
+  h3.style.color = color;
+  //accessing the h4 element
+  let h4 = document.querySelector("h4");
+  h4.style.color = color;
+}
+function reset() {
+  //accessing the h1 element
+  let color = "";
+  let h1 = document.querySelector("h1");
+  h1.style.color = color;
+  //accessing the h3 element
+  let h3 = document.querySelector("h3");
+  h3.style.color = color;
+  //accessing the h4 element
+  let h4 = document.querySelector("h4");
+  h4.style.color = color;
+}
